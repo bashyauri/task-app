815191e16ce917204219ab1e1b99fa81b2a96fc3
Bashar Umar <basharu83@gmail.com>
2025-01-02 15:55:48 +0100

    refactor api

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::post(uri: '/register', action: [AuthController::class, 'register']);
 Route::post(uri: '/login', action: [AuthController::class, 'login']);
-Route::get(uri: '/projects', action: [ProjectController::class, 'index']);
-Route::post(uri: '/projects', action: [ProjectController::class, 'store']);
-Route::put(uri: '/projects', action: [ProjectController::class, 'update']);
-Route::post(uri: '/projects/pinned', action: [ProjectController::class, 'pinnedProject']);
+
+Route::controller(controller: ProjectController::class)->group(function () {
+    Route::get(uri: '/projects', action: 'index');
+    Route::post(uri: '/projects', action: 'store');
+    Route::put(uri: '/projects', action: 'update');
+    Route::post(uri: '/projects/pinned', action: 'pinnedProject');
+});
+
 Route::get(uri: '/user', action: function (Request $request) {
     return $request->user();
 })->middleware(middleware: 'auth:sanctum');
