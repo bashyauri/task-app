05e3d4fc898ab51b681fbdd762e547651f94bce8
Bashar Umar <basharu83@gmail.com>
2025-01-07 15:37:12 +0100

    configure vue

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,15 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get(uri: '/', action: function () {
-    return view(view: 'welcome');
+// Route::get(uri: '/', action: function () {
+//     return view(view: 'welcome');
+// });
+Route::get('/', function () {
+    return redirect('/app/login');
 });
+
+Route::get('/app/{any}', function () {
+    return view('welcome');
+});
+
 Route::get(uri: '/check_email/{token}', action: [AuthController::class, 'validateEmail']);
